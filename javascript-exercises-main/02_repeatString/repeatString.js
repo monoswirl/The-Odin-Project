e9498238d18d23f86a494385f72e008d19f3ff1d
monoswirl <monoswirl@gmail.com>
2024-12-20 15:36:53 -0600

    Complete exercises 1 & 2

--- javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    if (num == 0) {
+        return result;
+    }
+    
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
